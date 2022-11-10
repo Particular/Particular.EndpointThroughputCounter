77f56f94cab57cbee108720c94a46a9d3efa6c72
David Boike <david.boike@gmail.com>
2022-11-10 13:34:05 -0600

    Try to deal with case where ServiceControl is not reliably returning data (#75)

---
+++ src/Tool/Infra/ServiceControlDataException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+partial class ServiceControlCommand
+{
+    class ServiceControlDataException : Exception
+    {
+        public string Url { get; }
+        public int Attempts { get; }
+
+        public ServiceControlDataException(string url, int tryCount, Exception inner)
+            : base(inner.Message, inner)
+        {
+            Url = url;
+            Attempts = tryCount;
+        }
+    }
+}
