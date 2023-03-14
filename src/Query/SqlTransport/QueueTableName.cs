817bfb536fedafdd3fd8ff12daf130e44a978cc4
David Boike <david.boike@gmail.com>
2023-03-14 11:23:14 -0500

    Separate files

--- src/Query/SqlTransport/QueueTableName.cs
+++ src/Query/SqlTransport/QueueTableName.cs
@@ -20,27 +20,5 @@
 
         public string DisplayName => $"[{DatabaseName}].{FullName}";
     }
-
-    public class QueueTableSnapshot : QueueTableName
-    {
-        public QueueTableSnapshot(QueueTableName details)
-            : base(details.DatabaseName, details.Schema, details.Name)
-        {
-
-        }
-
-        public long? RowVersion { get; set; }
-    }
-
-    public class QueueTableThroughput : QueueTableName
-    {
-        public QueueTableThroughput(QueueTableName table, long throughput)
-            : base(table.DatabaseName, table.Schema, table.Name)
-        {
-            Throughput = throughput;
-        }
-
-        public long Throughput { get; }
-    }
 }
 
