435a72ba611c8ae9ee8c61ed394466c12e5810e2
David Boike <david.boike@gmail.com>
2023-09-20 12:09:17 -0500

    Deal with non-bug bugs currently in Raygun (#602)
    * Deal with things like "SSL connection could not be established"
    
    * Check URI validity
    
    * Detect when stdin is not available
    
    * Thanks for the super-useful SqlExcpetion number
    
    * Another database not found
    
    * When Azure resource group can't be found
    
    * Update src/AppCommon/Infra/InteractiveHttpAuth.cs
    
    Co-authored-by: Adam Wright <awright18@gmail.com>
    
    ---------
    
    Co-authored-by: Adam Wright <awright18@gmail.com>

--- src/AppCommon/Infra/InteractiveHttpAuth.cs
+++ src/AppCommon/Infra/InteractiveHttpAuth.cs
@@ -14,7 +14,17 @@
 
         public static async Task<Func<HttpClient>> CreateHttpClientFactory(Uri authUri, int maxTries = 3, Action<HttpClient> configureNewClient = null, CancellationToken cancellationToken = default)
         {
-            var uriPrefix = new Uri(authUri.GetLeftPart(UriPartial.Authority));
+            Uri uriPrefix = null;
+
+            try
+            {
+                uriPrefix = new Uri(authUri.GetLeftPart(UriPartial.Authority));
+            }
+            catch (UriFormatException)
+            {
+                throw new HaltException(HaltReason.InvalidConfig, $"The URL '{authUri}' is invalid. It must be fully-formed, including http:// or https://.");
+            }
+
             var credentials = new CredentialCache();
 
             NetworkCredential credential = null;
@@ -51,43 +61,50 @@
                 var http = new HttpClient(socketHandler, disposeHandler: false);
                 configureNewClient?.Invoke(http);
 
-                using var response = await http.GetAsync(authUri, cancellationToken);
-
                 try
                 {
-                    _ = response.EnsureSuccessStatusCode();
+                    using var response = await http.GetAsync(authUri, cancellationToken);
 
-                    return () => new HttpClient(socketHandler, disposeHandler: false);
+                    try
+                    {
+                        _ = response.EnsureSuccessStatusCode();
+
+                        return () => new HttpClient(socketHandler, disposeHandler: false);
+                    }
+                    catch (HttpRequestException x) when (response.StatusCode == HttpStatusCode.Unauthorized)
+                    {
+                        socketHandler.Dispose();
+                        if (--maxTries <= 0)
+                        {
+                            throw new HaltException(HaltReason.Auth, $"Unable to authenticate to {uriPrefix}", x);
+                        }
+
+                        Out.WriteLine();
+                        Out.WriteLine($"Unable to access {uriPrefix} as {currentUser ?? "default credentials"}.");
+                        Out.WriteLine("Allowed authentication methods are:");
+                        foreach (var authHeader in response.Headers.WwwAuthenticate)
+                        {
+                            Out.WriteLine($"  * {authHeader.Scheme} ({authHeader.Parameter})");
+                        }
+                        Out.WriteLine();
+
+                        Out.WriteLine($"Enter authentication for {uriPrefix}:");
+                        Out.Write("Username: ");
+                        currentUser = Out.ReadLine();
+                        Out.Write("Password: ");
+                        var pass = Out.ReadPassword();
+
+                        credential = new NetworkCredential(currentUser, pass);
+                        var newSchemes = response.Headers.WwwAuthenticate.Select(h => h.Scheme).ToArray();
+                        if (newSchemes.Any())
+                        {
+                            schemes = newSchemes;
+                        }
+                    }
                 }
-                catch (HttpRequestException x) when (response.StatusCode == HttpStatusCode.Unauthorized)
+                catch (HttpRequestException x)
                 {
-                    socketHandler.Dispose();
-                    if (--maxTries <= 0)
-                    {
-                        throw new HaltException(HaltReason.Auth, "Unable to authenticate to " + uriPrefix, x);
-                    }
-
-                    Out.WriteLine();
-                    Out.WriteLine($"Unable to access {uriPrefix} as {currentUser ?? "default credentials"}.");
-                    Out.WriteLine("Allowed authentication methods are:");
-                    foreach (var authHeader in response.Headers.WwwAuthenticate)
-                    {
-                        Out.WriteLine($"  * {authHeader.Scheme} ({authHeader.Parameter})");
-                    }
-                    Out.WriteLine();
-
-                    Out.WriteLine($"Enter authentication for {uriPrefix}:");
-                    Out.Write("Username: ");
-                    currentUser = Out.ReadLine();
-                    Out.Write("Password: ");
-                    var pass = Out.ReadPassword();
-
-                    credential = new NetworkCredential(currentUser, pass);
-                    var newSchemes = response.Headers.WwwAuthenticate.Select(h => h.Scheme).ToArray();
-                    if (newSchemes.Any())
-                    {
-                        schemes = newSchemes;
-                    }
+                    throw new HaltException(HaltReason.InvalidConfig, $"Unable to connect to '{authUri}'. Are you sure you have the correct URL? Original error message was: {x.Message}");
                 }
             }
         }
