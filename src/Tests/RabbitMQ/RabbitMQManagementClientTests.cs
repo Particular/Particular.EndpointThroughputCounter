0ce9a2f30d0fbbf93790a48d5f2b2e77c226561f
danielmarbach <danielmarbach@users.noreply.github.com>
2024-10-02 17:38:07 +0200

    More test scenarios for wider coverage to leave the camping ground cleaner than it was before

--- src/Tests/RabbitMQ/RabbitMQManagementClientTests.cs
+++ src/Tests/RabbitMQ/RabbitMQManagementClientTests.cs
@@ -4,6 +4,7 @@ namespace Particular.ThroughputQuery.RabbitMQ.Tests
     using System.Net.Http;
     using System.Threading;
     using System.Threading.Tasks;
+    using Approvals;
     using NUnit.Framework;
     using RabbitMQ;
 
@@ -71,7 +72,152 @@ namespace Particular.ThroughputQuery.RabbitMQ.Tests
             Assert.DoesNotThrowAsync(async () => await client.GetQueueDetails());
         }
 
-        class FakeHttpHandler : HttpClientHandler
+        [Test]
+        public async Task Should_fetch_queue_details()
+        {
+            SendCallback = _ =>
+            {
+                var response = new HttpResponseMessage
+                {
+                    Content = new StringContent("""
+                    {
+                        "items": [
+                            {
+                                "name": "queue1",
+                                "vhost": "vhost1",
+                                "memory": 1024,
+                                "message_stats": {
+                                    "ack": 1
+                                }
+                            },
+                            {
+                                "name": "queue2",
+                                "vhost": "vhost2",
+                                "message_stats": {
+                                    "ack": 2
+                                }
+                            },
+                            {
+                                "name": "queue3",
+                                "vhost": "vhost1"
+                            }
+                        ],
+                        "page": 1,
+                        "page_count": 1,
+                        "page_size": 500,
+                        "total_count": 2
+                    }
+                    """)
+                };
+                return response;
+            };
+
+            Approver.Verify(await client.GetQueueDetails());
+        }
+
+        [Test]
+        public async Task Should_fetch_queue_details_in_old_format()
+        {
+            SendCallback = _ =>
+            {
+                var response = new HttpResponseMessage
+                {
+                    Content = new StringContent("""
+                    [
+                        {
+                            "name": "queue1",
+                            "vhost": "vhost1",
+                            "memory": 1024,
+                            "message_stats": {
+                                "ack": 1
+                            }
+                        },
+                        {
+                            "name": "queue2",
+                            "vhost": "vhost2",
+                            "message_stats": {
+                                "ack": 2
+                            }
+                        },
+                        {
+                            "name": "queue3",
+                            "vhost": "vhost1"
+                        }
+                    ]
+                    """)
+                };
+                return response;
+            };
+
+            Approver.Verify(await client.GetQueueDetails());
+        }
+
+        [Test]
+        public async Task Should_fetch_queue_details_with_paging()
+        {
+            SendCallback = request =>
+            {
+                HttpResponseMessage response = null;
+
+                if (request.RequestUri.ToString().Contains("page=1"))
+                {
+                    response = new HttpResponseMessage
+                    {
+                        Content = new StringContent("""
+                        {
+                            "items": [
+                                {
+                                    "name": "queue1",
+                                    "vhost": "vhost1",
+                                    "memory": 1024,
+                                    "message_stats": {
+                                        "ack": 1
+                                    }
+                                }
+                            ],
+                            "page": 1,
+                            "page_count": 2,
+                            "page_size": 500,
+                            "total_count": 2
+                        }
+                        """)
+                    };
+                }
+                else if (request.RequestUri.ToString().Contains("page=2"))
+                {
+                    response = new HttpResponseMessage
+                    {
+                        Content = new StringContent("""
+                        {
+                            "items": [
+                                {
+                                    "name": "queue2",
+                                    "vhost": "vhost2",
+                                    "message_stats": {
+                                        "ack": 2
+                                    }
+                                }
+                            ],
+                            "page": 2,
+                            "page_count": 2,
+                            "page_size": 500,
+                            "total_count": 2
+                        }
+                        """)
+                    };
+                }
+                else
+                {
+                    throw new Exception();
+                }
+
+                return response;
+            };
+
+            Approver.Verify(await client.GetQueueDetails());
+        }
+
+        sealed class FakeHttpHandler : HttpClientHandler
         {
             public Func<HttpRequestMessage, HttpResponseMessage> SendCallback { get; set; }
 
