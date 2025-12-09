2ba0fe7f8940e9d4399e0b2f462676037818d617
Andreas Öhlund <andreas.ohlund@particular.net>
2025-12-09 20:44:44 +0100

    Better output for azure service bus results

--- src/AppCommon/Commands/AzureServiceBusCommand.cs
+++ src/AppCommon/Commands/AzureServiceBusCommand.cs
@@ -12,29 +12,17 @@ class AzureServiceBusCommand : BaseCommand
 
         var resourceIdArg = new Option<string>(
             name: "--resourceId",
-            description: "The resource id for the Azure Service Bus namespace, which can be found in the Properties page in the Azure Portal.")
-        {
-            IsRequired = true
-        };
+            description: "The resource id for the Azure Service Bus namespace, which can be found in the Properties page in the Azure Portal.") { IsRequired = true };
 
         var serviceBusDomainArg = new Option<string>("--serviceBusDomain",
-            description: "The Service Bus domain. Defaults to 'servicebus.windows.net' and only must be specified for Azure customers using non-standard domains like government cloud customers.")
-        {
-            IsRequired = false
-        };
+            description: "The Service Bus domain. Defaults to 'servicebus.windows.net' and only must be specified for Azure customers using non-standard domains like government cloud customers.") { IsRequired = false };
 
         var regionArg = new Option<string>(
             name: "--region",
-            description: "The Azure region where the Service Bus namespace is located, which is listed as the location in the Properties page in the Azure Portal.")
-        {
-            IsRequired = true
-        };
+            description: "The Azure region where the Service Bus namespace is located, which is listed as the location in the Properties page in the Azure Portal.") { IsRequired = true };
 
         var metricsDomainArg = new Option<string>("--metricsDomain",
-            description: "The Azure Monitor Metrics domain. Defaults to 'metrics.monitor.azure.com' and only must be specified for Azure customers using non-standard domains like government cloud customers.")
-        {
-            IsRequired = false
-        };
+            description: "The Azure Monitor Metrics domain. Defaults to 'metrics.monitor.azure.com' and only must be specified for Azure customers using non-standard domains like government cloud customers.") { IsRequired = false };
 
         serviceBusDomainArg.SetDefaultValue("servicebus.windows.net");
         metricsDomainArg.SetDefaultValue("metrics.monitor.azure.com");
@@ -74,7 +62,7 @@ class AzureServiceBusCommand : BaseCommand
     string[] queueNames;
 
     public AzureServiceBusCommand(SharedOptions shared, string resourceId, string serviceBusDomain, string region, string metricsDomain)
-    : base(shared)
+        : base(shared)
     {
         azure = new AzureClient(resourceId, serviceBusDomain, region, metricsDomain, Out.WriteLine);
         RunInfo.Add("AzureServiceBusNamespace", azure.FullyQualifiedNamespace);
@@ -84,7 +72,7 @@ class AzureServiceBusCommand : BaseCommand
     {
         try
         {
-            var endTime = DateOnly.FromDateTime(DateTime.UtcNow).AddDays(-1);
+            var endTime = DateOnly.FromDateTime(DateTime.UtcNow);
             var startTime = endTime.AddDays(-90);
             var results = new List<QueueThroughput>();
 
@@ -95,40 +83,51 @@ class AzureServiceBusCommand : BaseCommand
             {
                 var queueName = queueNames[i];
 
-                Out.WriteLine($"Gathering metrics for queue {i + 1}/{queueNames.Length}: {queueName}");
+                Out.Write($"Gathering metrics for queue {i + 1}/{queueNames.Length}: {queueName}");
 
                 var metricValues = (await azure.GetMetrics(queueName, startTime, endTime, cancellationToken)).OrderBy(m => m.TimeStamp).ToArray();
 
-                if (metricValues is not null)
+                var maxThroughput = metricValues.Select(timeEntry => timeEntry.Total).Max();
+
+                // Since we get 90 days of data, if there's no throughput in that amount of time, hard to legitimately call it an endpoint
+                if (maxThroughput is not null and not 0)
                 {
-                    var maxThroughput = metricValues.Select(timeEntry => timeEntry.Total).Max();
-
-                    // Since we get 90 days of data, if there's no throughput in that amount of time, hard to legitimately call it an endpoint
-                    if (maxThroughput is not null and not 0)
+                    var start = DateOnly.FromDateTime(metricValues.First().TimeStamp.UtcDateTime);
+                    var end = DateOnly.FromDateTime(metricValues.Last().TimeStamp.UtcDateTime);
+                    var currentDate = start;
+                    var data = new Dictionary<DateOnly, DailyThroughput>();
+                    while (currentDate <= end)
                     {
-                        var start = DateOnly.FromDateTime(metricValues.First().TimeStamp.UtcDateTime);
-                        var end = DateOnly.FromDateTime(metricValues.Last().TimeStamp.UtcDateTime);
-                        var currentDate = start;
-                        var data = new Dictionary<DateOnly, DailyThroughput>();
-                        while (currentDate <= end)
+                        data.Add(currentDate, new DailyThroughput
                         {
-                            data.Add(currentDate, new DailyThroughput { MessageCount = 0, DateUTC = currentDate });
+                            MessageCount = 0,
+                            DateUTC = currentDate
+                        });
 
-                            currentDate = currentDate.AddDays(1);
-                        }
-
-                        foreach (var metricValue in metricValues)
-                        {
-                            currentDate = DateOnly.FromDateTime(metricValue.TimeStamp.UtcDateTime);
-                            data[currentDate] = new DailyThroughput { MessageCount = (long)(metricValue.Total ?? 0), DateUTC = currentDate };
-                        }
-
-                        results.Add(new QueueThroughput { QueueName = queueName, Throughput = (long?)maxThroughput, DailyThroughputFromBroker = [.. data.Values] });
+                        currentDate = currentDate.AddDays(1);
                     }
-                    else
+
+                    foreach (var metricValue in metricValues)
                     {
-                        Out.WriteLine(" - No throughput detected in 90 days, ignoring");
+                        currentDate = DateOnly.FromDateTime(metricValue.TimeStamp.UtcDateTime);
+                        data[currentDate] = new DailyThroughput
+                        {
+                            MessageCount = (long)(metricValue.Total ?? 0),
+                            DateUTC = currentDate
+                        };
                     }
+
+                    results.Add(new QueueThroughput
+                    {
+                        QueueName = queueName,
+                        Throughput = (long?)maxThroughput,
+                        DailyThroughputFromBroker = [.. data.Values]
+                    });
+                    Out.WriteLine($" - MaxThroughput: {maxThroughput}");
+                }
+                else
+                {
+                    Out.WriteLine(" - No throughput detected in 90 days, ignoring");
                 }
             }
 
